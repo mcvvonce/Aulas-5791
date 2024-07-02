96c261a39ef4020d48fbe09894e8bb1356fd16b3
Yun Li ' Version <166619941+mcvvonce@users.noreply.github.com>
2024-07-02 10:24:42 -0300

    Delete Aula 2 directory

--- Aula 2/index.js
+++
@@ -1,9 +0,0 @@
-const readlinesync = require('readline-sync');
-
-const nu1 = Number(readlinesync.question('insira o texto'))
-const nu2 = Number(readlinesync.question('insira o texto'))
-
-
-console.log(nu1 + nu2)
-
-//npm install readline-sync
