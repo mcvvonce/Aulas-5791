9f94bf39420a37e6b274e51a6cbe70e972521b14
Yun Li ' Version <166619941+mcvvonce@users.noreply.github.com>
2024-07-02 11:45:46 -0300

    Add files via upload

---
+++ Aula 2/index.js
@@ -0,0 +1,9 @@
+const readlinesync = require('readline-sync');
+
+const nu1 = Number(readlinesync.question('insira o texto'))
+const nu2 = Number(readlinesync.question('insira o texto'))
+
+
+console.log(nu1 + nu2)
+
+//npm install readline-sync
