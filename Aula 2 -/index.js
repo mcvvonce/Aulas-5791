e5ca9799a57688bc2ae9df08fbcced1f5aeb9c72
Yun Li ' Version <166619941+mcvvonce@users.noreply.github.com>
2024-07-02 11:35:16 -0300

    Delete Aula 2 - directory

--- Aula 2 -/index.js
+++
@@ -1,9 +0,0 @@
-const readlinesync = require('readline-sync')
-
-const nu1 = Number(readlinesync.question('insira nota'))
-const nu2 = Number(readlinesync.question('insira outra nota'))
-const nu3 = Number(readlinesync.question('insira mais uma nota'))
-
-
-console.log(nu1 + nu2 + nu3)
-
