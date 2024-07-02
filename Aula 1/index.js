72e69d2bed4b2c9f52e287f3db954af746b87a57
Yun Li ' Version <166619941+mcvvonce@users.noreply.github.com>
2024-07-02 10:22:11 -0300

    Add files via upload

---
+++ Aula 1/index.js
@@ -0,0 +1,10 @@
+const nome = "Abobora" 
+const sobrenome = "Silveira" 
+const idade = 15
+const estudante = true
+
+
+console.log("Nome:", nome)
+console.log("Sobrenome:", sobrenome)
+console.log("Idade:", idade)
+console.log("Estudante:", estudante)
